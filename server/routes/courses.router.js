f16665d5a51162601150a32e02992ca09d5b6d31
Matt Lissick <mattlissick@gmail.com>
2019-09-10 13:00:42 -0500

    get route for courses working with postman

--- server/routes/courses.router.js
+++ server/routes/courses.router.js
@@ -3,8 +3,14 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    
-})
+    const queryText = 'SELECT id, name, description FROM course';
+    pool.query(queryText)
+        .then((result) => {res.send(result.rows);})
+        .catch((error) => {
+            console.log('Error completing SELECT course query', error);
+            res.sendStatus(500);
+        });
+});
 
 
 

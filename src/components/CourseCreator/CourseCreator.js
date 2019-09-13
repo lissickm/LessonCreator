6169787af1e11ddc12f2da6c5db4e74689a23b2f
Matt Lissick <mattlissick@gmail.com>
2019-09-13 11:39:49 -0500

    course creator is updating state when typing in input fields

--- src/components/CourseCreator/CourseCreator.js
+++ src/components/CourseCreator/CourseCreator.js
@@ -3,26 +3,47 @@ import { connect } from 'react-redux';
 
 class CourseCreator extends Component {
 
+    state = {
+        courseName: "",
+        courseDescription: ""
+    }
+
     handleBackClick = () => {
         this.props.history.push('/coursesAvailable');
     }
 
+    handleInputChange = (propertyName, event) => {
+        console.log('in handle name change')
+        this.setState({
+
+            ...this.state,
+
+            [propertyName]: event.target.value,
+
+        })
+    }
+
+
+
+    // <input type='text' value={this.state.name} placeholder='name' onChange={(event) => { this.handleNameChange('name', event) }} />
+    //     <input type='text' value={this.state.kingdom} placeholder='kingdom' onChange={(event) => { this.handleNameChange('kingdom', event) }} />
     render() {
 
 
         return(
             <div>
                 <h1>New Course Creator</h1>
+                {JSON.stringify(this.state)}
                 <p>Please enter the name of your course below. Also enter a detailed description of your course, including all topics covered.</p>
                 <br/>
                 <form>
                     <label>
                         Course Name:
-                        <input type="text" name="name" />
+                        <input type="text" value={this.state.courseName} onChange={(event) => {this.handleInputChange('courseName', event)}} />
                     </label>
                     <label>
                         Course Description:
-                        <input type="text" name="name" />
+                        <input type="text" value={this.state.courseDescription} onChange={(event) => { this.handleInputChange('courseDescription', event) }} />
                     </label>
                     <input type="submit" value="Submit" />
                 </form>

0e9c770a3ec5a8402519adf53d09f2d6705fcadb
Matt Lissick <mattlissick@gmail.com>
2019-09-17 10:12:18 -0500

    added an edit lessons saga

---
+++ src/redux/sagas/editLessonSaga.js
@@ -0,0 +1,22 @@
+import { put, takeEvery } from 'redux-saga/effects';
+import axios from 'axios';
+
+function * editLesson(action) {
+    try {
+        yield axios.put(`api/lessons/${action.payload.id}`, action.payload);
+        
+
+    } catch (error) {
+        console.log('error in edit lesson: ', error)
+    }
+}
+
+function* editLessonSaga() {
+    yield takeEvery('CHANGE_LESSON_INFORMATION', editLesson)
+
+}
+
+
+
+
+export default editLessonSaga;
